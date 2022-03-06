e425ac7b33583761db77c59d64900311b1c86477
Sergio <tobito.8897@gmail.com>
2022-03-06 01:37:25 -0600

    typo

--- ksh/download_siena.sh
+++ ksh/download_siena.sh
@@ -2,7 +2,7 @@
 SCRIPTPATH=`pwd`
 DATAPATH=`echo /../../Data/SienaRaw`
 cd $SCRIPTPATH$DATAPATH
-#wget -r -N -c -np https://physionet.org/files/siena-scalp-eeg/1.0.0/
+wget -r -N -c -np https://physionet.org/files/siena-scalp-eeg/1.0.0/
 cd "./physionet.org/files/siena-scalp-eeg/1.0.0"
 cp -rv * ../../../../
 cd $SCRIPTPATH$DATAPATH
