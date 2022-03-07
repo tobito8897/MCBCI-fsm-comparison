302bcd1944da9e61cc52cc85bc9f0842cf9044f7
Sergio <tobito.8897@gmail.com>
2022-03-06 20:59:29 -0600

    Comments updated

--- bin/grid_search.py
+++ bin/grid_search.py
@@ -7,19 +7,9 @@ Options:
     --feature_set=<fs>    1 or 2
 """
 # 1.- Read EDF Files
-# 2.- Keep needed channels based on channels.txt
-# 3.- Scale EEG signal (original data is in Volts)
-# 4.- Filter out drift (<0.5Hz)
-# 5.- Generate X number of windows of Y length per each category
-#     ["ictal", "noictal"]
-# 6.- Get bands from EEG (delta, theta, alpha, beta)
-# 7.- Calculate parameters for each window
-#     feature_set_1 = (peak_freq, median_freq, variance, rms, skewness, kurtosis, Zero Crossing, Range)
-#     feature_set_1 = (peak_freq, median_freq, skewness, kurtosis, SampEn, min value, mean, std)
-# 8.- Split 0.1 data for grid search and 0.9 for rest of analysis process
-# 9.- Save data
-# Note: check features_map.json for understanding features order.
-
+# 2.- Read the list of parameters to test from configuration file
+# 3.- Test each parameter combination by using Cross Validation
+# 4.- Ssave the results
 import sys
 import logging
 import numpy as np
