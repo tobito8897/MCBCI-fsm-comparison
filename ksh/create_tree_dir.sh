8ef4ddbf47cdebf605de4b6e5a0fe75b393133cb
Sergio <tobito.8897@gmail.com>
2022-03-08 22:54:46 -0600

    Script for creating the tree directory

---
+++ ksh/create_tree_dir.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+SCRIPTPATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DATAPATH=`echo /../..`
+cd $SCRIPTPATH$DATAPATH
+
+mkdir -p "Data/ChbmitRaw"
+mkdir -p "Data/SienaRaw"
+
+mkdir -p "processeddata/ProcessedDataChbMit_fs1/Common/Ictal"
+mkdir -p "processeddata/ProcessedDataChbMit_fs1/Common/NoIctal"
+
+mkdir -p "processeddata/ProcessedDataChbMit_fs1/GridSearch/Ictal"
+mkdir -p "processeddata/ProcessedDataChbMit_fs1/GridSearch/NoIctal"
+
+mkdir -p "processeddata/ProcessedDataSiena_fs1/Common/Ictal"
+mkdir -p "processeddata/ProcessedDataSiena_fs2/Common/NoIctal"
