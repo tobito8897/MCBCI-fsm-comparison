b0f7029c01e4a02d5896f13d77f42591c237ebb5
Sergio <tobito.8897@gmail.com>
2022-03-08 23:37:52 -0600

    Changes to run in LeoAtrox

---
+++ ksh/run_leoatrox_job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source venv/bin/activate
+SCRIPTPATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DATAPATH=`echo /../bin`
+cd $SCRIPTPATH$DATAPATH
+
+/lustre/home/ssanchez/python-core_375/bin/python3 calculate_generic_windows_parameters_chb.py --feature_set=2
+deactivate
