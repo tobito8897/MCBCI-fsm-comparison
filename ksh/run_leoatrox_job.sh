2ea17b25f5554723144998318f4faca37af99b71
Sergio <tobito.8897@gmail.com>
2022-03-10 00:38:00 -0600

    Intructions to run in LeoAtrox

--- ksh/run_leoatrox_job.sh
+++ ksh/run_leoatrox_job.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DATAPATH=`echo /../bin`
 cd $SCRIPTPATH$DATAPATH
 
-/lustre/home/ssanchez/python-core_375/bin/python3 calculate_generic_windows_parameters_chb.py --feature_set=2
+/lustre/home/ssanchez/python-core_375/bin/python3 calculate_generic_windows_parameters_chb-mit.py --feature_set=2
 deactivate
